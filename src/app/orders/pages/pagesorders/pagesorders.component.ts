0b3a386da74b62c8197bec19ea53a730b94961d5
jeanmarc123456 <j.delfosse@me.com>
2020-09-23 12:13:56 +0200

    creation des services clients et orders. Création des interfaces et modeles clients et orders. Ajout de la résupération des données serveur pour clients et orders dans leurs composants respectifs. Création d'un enum. Création de la mise en place de l'url

--- src/app/orders/pages/pagesorders/pagesorders.component.ts
+++ src/app/orders/pages/pagesorders/pagesorders.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Order } from 'src/app/shared/models/order';
+import { OrderService } from '../../services/order.service';
 
 @Component({
   selector: 'app-pagesorders',
@@ -7,9 +9,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PagesordersComponent implements OnInit {
 
-  constructor() { }
+  private subscription: any;
+  constructor(private orderservice: OrderService) { }
 
   ngOnInit(): void {
+    this.subscription = this.orderservice.collection.subscribe(
+      (datas) => {
+        console.log(datas);
+        let order = new Order(datas[0]);
+        console.log(order.totalHt())
+      })
+
   }
 
 }
