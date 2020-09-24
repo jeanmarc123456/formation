0da2da548a0c4410d59a838c369db531ba27c971
jeanmarc123456 <j.delfosse@me.com>
2020-09-24 11:32:26 +0200

    Création et utilisation d'une directive personnalisée

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,13 +1,14 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { TableLightComponent } from './components/table-light/table-light.component';
+import { StateDirective } from './directives/state.directive';
 import { TotalPipe } from './pipes/total.pipe';
 import { SharedRoutingModule } from './shared-routing.module';
 
 
 
 @NgModule({
-  declarations: [TableLightComponent, TotalPipe],
+  declarations: [TableLightComponent, TotalPipe, StateDirective],
   imports: [
     CommonModule,
     SharedRoutingModule
@@ -15,6 +16,7 @@ import { SharedRoutingModule } from './shared-routing.module';
   exports: [
     TableLightComponent,
     TotalPipe,
+    StateDirective
   ]
 })
 export class SharedModule { }
